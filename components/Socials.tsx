e4e902514e4bc5dcc59ba41b7197edfdc532ef2a
Никита Воронин <nikitavoronin@MacBook-Pro-Nikita-2.local>
2023-10-21 17:14:08 +0800

    adjusted socials logos

--- components/Socials.tsx
+++ components/Socials.tsx
@@ -6,13 +6,21 @@ import { IconContext } from 'react-icons';
 const Socials = () => {
   return (
     <div>
-      <p className='text-white text-2xl ml-[87%]'> My socials:</p>
-      <div className='flex flex-row mt-1 ml-[75%] gap-5'>
+      <p className='text-white text-2xl ml-[5%]'> My socials:</p>
+      <div className='flex flex-row mt-2 ml-[0%] gap-5'>
         <IconContext.Provider value={{ color: 'white', className: '', size: '3em' }}>
+              <Link href='https://www.linkedin.com/in/nikita-voronin-4b6758293/'>
               <FaLinkedinIn />
+              </Link>
+              <Link href='https://www.instagram.com/loground'>
               <FaInstagramSquare />
+              </Link>
+              <Link href='https://twitter.com/nickvrnn'>
               <FaTwitterSquare />
+              </Link>
+              <Link href='https://github.com/loground'>
               <FaGithubSquare />
+              </Link>
         </IconContext.Provider>
       </div>
     </div>
