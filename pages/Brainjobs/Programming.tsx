90c81d3c291006ae9b70585101611cd70e0f9345
Никита Воронин <nikitavoronin@MacBook-Pro-Nikita-2.local>
2023-11-14 11:14:10 +0800

    fixed programming section

--- pages/Brainjobs/Programming.tsx
+++ pages/Brainjobs/Programming.tsx
@@ -27,21 +27,23 @@ const textContent = [
   {
     id: 2,
     content: (
-      <div className="flex flex-col md:flex-row gap-10 justify-center px-4 py-16 bg-base-200">
+      <div className="flex flex-col gap-6 justify-center px-4 py-10 bg-base-200">
         <p className="text-black">
           I quickly progressed to learning JavaScript fundamentals, diving into arrays, objects, and
           basic functions. My study routine included reading materials, watching YouTube tutorials,
           practicing on Hexlet, and completing tasks from my mentor. I dedicated several months to
           mastering these core concepts, spending my days solving a variety of coding challenges.
         </p>
-        <img src="/programming/2.png" className="h-30"></img>
+        <img
+          src="/programming/2.png"
+          className="h-30 md:h-[50%] md:w-[30%] md:mt-10 md:ml-[35%]"></img>
       </div>
     ),
   },
   {
     id: 3,
     content: (
-      <div className="flex flex-col gap-5 justify-center px-4 py-16 bg-base-200">
+      <div className="flex flex-col md:flex-row gap-5 justify-center px-4 py-16 bg-base-200">
         <p className="text-black">
           Moving on to React, I initially found it challenging to grasp its syntax, and the
           complexity of the information from Hexlet.io added to the difficulty. To tackle this, I
@@ -49,7 +51,7 @@ const textContent = [
           beginners. There, I began with learning functional components after basic learning of
           class components from Hexlet.
         </p>
-        <img src="/programming/3.png" className="h-30"></img>
+        <img src="/programming/3.png" className="h-30 md:h-40"></img>
       </div>
     ),
   },
@@ -135,7 +137,7 @@ const Programming = () => {
         </ul>
       </div>
       <div className="mockup-window border bg-base-300">
-        <div className="flex justify-center px-4 py-16 bg-base-200 text-xl text-black">
+        <div className="flex justify-center px-4 py-10 bg-base-200 text-xl text-black">
           {textContent[currentStep].content}
         </div>
       </div>
