fd8d4227ed773a666c682a1928660656a4a1fe60
Никита Воронин <nickvoroninwork@gmail.com>
2025-04-17 12:03:37 +0800

    removed suspense

--- components/Features/MainPage/Hero.tsx
+++ components/Features/MainPage/Hero.tsx
@@ -17,44 +17,36 @@ const Hero = () => {
 
   return (
     <div className="relative h-screen items-center">
-      <Suspense
-        fallback={
-          <div
-            className="flex items-center justify-center text-white text-xl"
-            style={{ height: '85%' }}>
-            Get Ready!
-          </div>
-        }>
-        <ScrambleIn isHovered={hovered} />
+      <ScrambleIn isHovered={hovered} />
 
-        <Canvas
-          key="hero-canvas"
-          gl={{ antialias: false }}
-          className="bg-white opacity-80"
-          style={{ height: '85%' }}>
-          <directionalLight />
-          <pointLight position={[0, 0, 4]} power={30.0} />
-          <pointLight position={[1, 0, 1]} power={20.0} />
-          <pointLight position={[-1, 0, 1]} power={20.0} />
+      <Canvas
+        key="hero-canvas"
+        gl={{ antialias: false }}
+        className="bg-white opacity-80"
+        style={{ height: '85%' }}>
+        <directionalLight />
+        <pointLight position={[0, 0, 4]} power={30.0} />
+        <pointLight position={[1, 0, 1]} power={20.0} />
+        <pointLight position={[-1, 0, 1]} power={20.0} />
 
-          <RightWall />
+        <RightWall />
 
-          <ambientLight intensity={0.4} />
+        <ambientLight intensity={0.4} />
 
-          <TwitterLogo />
-          <InstagramLogo />
-          <LinkedLogo />
-          <GithubLogo />
+        <TwitterLogo />
+        <InstagramLogo />
+        <LinkedLogo />
+        <GithubLogo />
 
-          <Model
-            onPointerEnter={() => setIsHovered(true)}
-            onPointerLeave={() => setIsHovered(false)}
-            position={[0, -1, 2.6]}
-          />
+        <Model
+          onPointerEnter={() => setIsHovered(true)}
+          onPointerLeave={() => setIsHovered(false)}
+          position={[0, -1, 2.6]}
+        />
 
-          <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
-        </Canvas>
-      </Suspense>
+        <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
+      </Canvas>
+
       <h1 className="absolute w-[100vw] h-[30vh] text-white font-xl p-5 text-4xl mt-[-25%] md:mt-[-6%] lg:mt-[-7%] rounded-2xl bg-black md:text-5xl md:w-[100%]">
         Nikita, 28, Frontend {'=>'}{' '}
         <a
